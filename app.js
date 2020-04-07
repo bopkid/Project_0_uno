e5b70866ad5d4be80f537218ab1f397e43f31b64
alwong <alwong125@gmail.com>
2020-04-07 14:58:50 -0700

    added mechanic to play cards

--- app.js
+++ app.js
@@ -637,23 +637,32 @@ const game = {
 
     // will  interpert the card the player just played 
     cardInterperter(card){
-        if(card.card_color === this.currentColor || card.card_number===this.currentNumber){ 
-        this.changeColor(card);
-        this.changeNumber(card);
+        
+        if(card[1]=== this.currentColor || (card[1] === 'black')){
+            if((card[1] === 'black')){
+                game.currentColor = 'black'
+            }
+            
+            return true
         }
+        else if(this.currentColor ==='black'){
+            this.currentColor = card[1]
+            return true
+        }
+      
+      
     },
     // check if different from currentcolor if so changed it to this
     changeColor(card){
-        if(card.card_color != this.currentColor){
-            this.currentColor = card.card_color;
-        }
+        console.log(card[1])
+      game.currentColor = card[1];
+      return true
 
     },
     // check if differnt from number if so changed it to this
     changeNumber(card){
-        if(card.card_number != this.currentNumber){
-            this.currentNumber = card.card_number;
-        }
+        console.log(card)
+       game.currentNumber = card
     },
     // select a random card from the deck
     randomCard(){
@@ -697,7 +706,7 @@ const game = {
     // check who has no card left declar that person is the victor
     // and show who has won
     checkForVictor(){
-
+        
     },
     // clear the game borad and show to play again?
     clearAndPlay(){
@@ -705,15 +714,6 @@ const game = {
     }
 
 }
-const test = {
-    card_number:'5',
-    card_color:'green'
-}
-game.cardInterperter(test)
-console.log(game.currentNumber);
-console.log(game.currentColor)
-
-console.log(game.deck.length);
 
 /* 
     player object should hold what the player should see and handle the player in a sense play the card 
@@ -723,14 +723,56 @@ console.log(game.deck.length);
     2. Uno a bool 
     method: 
     1. playerCard()
+    2.displayCard()
  */
 const player ={
     playerHand:[],
     hasUno: false,
 
     playCard(){
+        const card = document.querySelector('.hand');
+        card.addEventListener('click',(event)=>{
+         
+            console.log(event.target.classList)
+           if(game.cardInterperter(event.target.classList))
+           {
+               console.log('red')
+           }
+        })
+        
+    },
+    displayCard(){
+        
+        for(let i = 0; i < this.playerHand.length;i++){
+            const showCard = document.createElement('span');
+            
+            showCard.classList.add( 'player_hand');
 
+            if(this.playerHand[i].card_color === 'yellow'){
+                showCard.classList.add('yellow')
+            }
+            else if(this.playerHand[i].card_color === 'blue'){
+                showCard.classList.add('blue')
+            }
+            else if(this.playerHand[i].card_color === 'red'){
+                showCard.classList.add('red')
+            }
+            else if(this.playerHand[i].card_color === 'green'){
+                showCard.classList.add('green')
+            }
+            else if(this.playerHand[i].card_color === 'black'){
+                showCard.classList.add('black')
+            }
+           showCard.innerText = this.playerHand[i].card_number
+            console.log(showCard)
+            const hand = document.querySelector('.hand');
+            console.log(hand)
+            hand.appendChild(showCard)
+        
+        
+        }
     }
+    
 }
 
 /* 
@@ -741,6 +783,7 @@ const player ={
     2. Uno a bool 
     method: 
     1. playerCard()
+    2.displayCard()
  */
 
 class computer{
@@ -771,7 +814,28 @@ const computersList = {
 
 }
 computersList.generateComputerPlayers();
+computersList.generateComputerPlayers();
 game.dealStarterHand();
 
-console.log(player.playerHand)
-console.log(computersList.computerPlayers[0].playerHand)
+// console.log(player.playerHand)
+// console.log(computersList.computerPlayers[0].playerHand)
+// console.log(player.playerHand[0].card_color)
+
+player.displayCard();
+
+
+const card = document.querySelector('.hand');
+card.addEventListener('click',(event)=>{
+ 
+    
+   if(game.cardInterperter(event.target.classList))
+   {
+      game.changeNumber(event.target.innerText)
+   }
+   else if(game.currentNumber == event.target.innerText){
+       game.changeColor(event.target.classList)
+   }
+   console.log(game.currentColor);
+   console.log(game.currentNumber)
+})
+
